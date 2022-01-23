67a424832902019cdaf132db3efa32b6864cdca8
spaditha <48846489+spaditha@users.noreply.github.com>
2022-01-23 17:36:52 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -5,3 +5,19 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/spaditha/AI.git
 git push -u origin main
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import json
+import math
+import os
+import random
+import modeling
+import optimization
+import tokenization
+import six
+import numpy as np
+import tensorflow as tf
